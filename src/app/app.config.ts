d2d5dee80bbdd7f9882bd78f9cf4506d4d467c27
cimey <cumakilinc27@gmail.com>
2025-08-06 21:50:01 +0200

    changes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,69 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { AuthHttpInterceptor, AuthModule } from '@auth0/auth0-angular';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(
+      withInterceptorsFromDi() // <-- this enables interceptor
+    ),
+    importProvidersFrom(
+      AuthModule.forRoot({
+        domain: 'dev-pu-4qw93.us.auth0.com',
+        clientId: 'AVupjakLeQMMLb66JRFwKswkJxhrMtVF',
+        authorizationParams: {
+          redirect_uri: window.location.origin,
+          audience: 'https://short-story.ai',
+          scope: "openid profile email"
+        },
+        httpInterceptor: {
+          allowedList: [
+            {
+              uri: 'https://localhost:7048/api/*', // Adjust to your API endpoint
+              tokenOptions: {
+                authorizationParams: {
+                  audience: 'https://short-story.ai',
+                  scope: "openid profile email"
+                }
+              }
+            },
+            {
+              uri: 'http://localhost:3333/api/*', // Adjust to your API endpoint
+              tokenOptions: {
+                authorizationParams: {
+                  audience: 'https://short-story.ai',
+                  scope: "openid profile email"
+                }
+              }
+            },
+            {
+              uri: 'https://storyshortaiclone.onrender.com/api/*', // Adjust to your API endpoint
+              tokenOptions: {
+                authorizationParams: {
+                  audience: 'https://short-story.ai',
+                  scope: "openid profile email"
+                }
+              }
+            },
+            {
+              uri: 'https://storyshortaiclone-2-381336012870.europe-west1.run.app/api*', // Adjust to your API endpoint
+              tokenOptions: {
+                authorizationParams: {
+                  audience: 'https://short-story.ai',
+                  scope: "openid profile email"
+                }
+              }
+            }
+          ]
+        }
+      })
+    ),
+    { provide: HTTP_INTERCEPTORS, useClass: AuthHttpInterceptor, multi: true },
   ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { ScriptInputComponent } from './features/script/script-input/script-input.component';
+import { PreviewComponent } from './features/script/preview/preview.component';
+import { GenerateComponent } from './features/script/generate.component/generate.component';
+import { AuthGuard } from '@auth0/auth0-angular';
+import { inject } from '@angular/core';
+import { MainComponent } from './features/script/main.component/main.component';
+import { LandingComponent } from './features/script/landing.component/landing.component';
+import { VideoListComponent } from './features/script/video-list.component/video-list.component';
+export const routes: Routes = [
+    { path: '', redirectTo: '/landing', pathMatch: 'full' },
+    { path: 'landing', component: LandingComponent },
+    {
+             path: '', component: MainComponent, children: [
+            { path: 'input', component: ScriptInputComponent },
+            { path: 'preview', component: PreviewComponent },
+            { path: 'video-list', component: VideoListComponent },
+            { path: 'generate', component: GenerateComponent }], canActivate: [(next, state) => inject(AuthGuard).canActivate(next, state)]
+    }
+];
